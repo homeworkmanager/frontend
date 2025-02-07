2faeb325930f8af330dc81157e6c648d543c6bd9
DeStRo_ <viktor2005.gl@gmail.com>
2025-02-07 23:00:05 +0300

    modify navigation ui

--- src/components/modules/Router/constants/routes.ts
+++ src/components/modules/Router/constants/routes.ts
@@ -1,13 +1,13 @@
 const error = '*';
 const main = '/';
 
-const auth = '/auth';
-const journalMobile = '/journal-mobile';
-const journalDesktop = '/journal-desktop';
-const admin = '/admin';
-const moderatorMobile = '/moderator-mobile';
-const moderatorDesktop = '/moderator-desktop';
-const profile = '/profile';
-const note = '/note';
+const auth = main + 'auth';
+const journalMobile = main + 'journal-mobile';
+const journalDesktop = main + 'journal-desktop';
+const admin = main + 'admin';
+const moderatorMobile = main + 'moderator-mobile';
+const moderatorDesktop = main + 'moderator-desktop';
+const profile = main + 'profile';
+const note = main + 'note';
 
 export { error, main, auth, journalMobile, journalDesktop, admin, moderatorMobile, moderatorDesktop, profile, note };
