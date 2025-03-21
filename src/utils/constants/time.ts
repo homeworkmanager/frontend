ed217add8706f6de75c1d9da8d9feb341004a1e5
DeStRo_ <viktor2005.gl@gmail.com>
2025-03-22 00:39:10 +0300

    hotfix time

--- src/utils/constants/time.ts
+++ src/utils/constants/time.ts
@@ -1,16 +1,22 @@
-export const startDate: StartDateType = {
+import { pad } from '../helpers/pad';
+
+const startDate: StartDateType = {
   year: 2025,
   month: 2,
   day: 3
 };
 
-export const scheduleBeginDate = {
-  date: new Date('2025.02.03').toISOString(),
-  days: 161
-};
-
-export const semStart = {
+const semStart = {
   year: 2025,
   month: 2,
   day: 10
 };
+
+const scheduleBeginDate = {
+  date: new Date(
+    `${startDate.year}-${pad(startDate.month)}-${pad(startDate.day)}T00:00:00`.concat('.000+03:00')
+  ).toISOString(),
+  days: 161
+};
+
+export { startDate, semStart, scheduleBeginDate };
