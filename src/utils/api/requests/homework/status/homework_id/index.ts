37389b180c2e3b8fd472bdc780e8d4d708f7f8c6
DeStRo_ <viktor2005.gl@gmail.com>
2026-01-31 20:49:48 +0300

    feature/FRONTEND-47: fix endpoint's body

--- src/utils/api/requests/homework/status/homework_id/index.ts
+++ src/utils/api/requests/homework/status/homework_id/index.ts
@@ -5,4 +5,11 @@ type PostHomeworkStatusParams = Omit<ScheduleHomeworkElement, 'subjectName' | 'h
 export type PostHomeworkStatusConfig = AxiosRequestConfig<PostHomeworkStatusParams>;
 
 export const postHomeworkStatus = async ({ params, config }: PostHomeworkStatusConfig) =>
-  api.post<PostHomeworkStatusResponse>(`/homework/status/${params.homeworkID}`, params, config);
+  api.post<PostHomeworkStatusResponse>(
+    `/homework/status/${params.homeworkID}`,
+    {
+      homeworkID: params.homeworkID,
+      status: params.isCompleted //Это же надо было тупому бекендеру додуматься в зависимости от запроса по-разному называть поле, выполняющее одиннаковыую функцию...
+    },
+    config
+  );
